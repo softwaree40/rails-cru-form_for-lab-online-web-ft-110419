824763fd6bffc7c7b12f9c4b024189de50bfeb28
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-03 23:34:30 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,27 +1,36 @@
 class ArtistsController < ApplicationController
      def index 
         binding.pry
-      @artists = Artist.all
+      @artist = Artist.all
         
      end
      
      def show 
-       
+       @artist = Artist.find(params[:id]) 
      end
      
      def new 
-       
+       @artist = Artist .new 
      end
      
      def create 
-       
+      @artist = Arist.create(aritst_params(:name,:bio))
      end
      
      def edit 
-       
+        @artist = Artist.find(params[:id]) 
      end 
      
      def update 
+        @artist = Artist.find(params[:id])
+        @artist.update(aritst_params(:name,:bio))
+        redirect_to artist_path(@artist)
+     end
+     
+     private 
+     
+     def aritst_params(*args)
+        params.require(:artist).permit(*args)
        
      end
   

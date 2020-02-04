9aab1d20baf26ff0f8a3ab126a832112f1640eb9
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-04 06:25:07 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,29 +1,34 @@
 class SongsController < ApplicationController
-    class ArtistsController < ApplicationController
      def index 
-       
+        @song = Song.all 
      end
      
      def show 
-       
+        @song = Song.find(params[:id])
      end
      
      def new 
-       
+        @song = Song.new
      end
      
      def create 
-       
+        @song = Song.create(songs_params(:name,:artist_id,:genre_id))
+        redirect_to song_path(@song)
      end
      
      def edit 
-       
+       @song = Song.find(params[:id])
      end 
      
      def update 
-       
+        @song = Song.find(params[:id])
+        @song.update(songs_params(:name,:artist_id,:genre_id))
+        redirect_to song_path(@song)
      end
-  
-end
+   private
+   
+    def songs_params(*args)
+       params.require(:song).permit(*args)
+    end
   
 end
